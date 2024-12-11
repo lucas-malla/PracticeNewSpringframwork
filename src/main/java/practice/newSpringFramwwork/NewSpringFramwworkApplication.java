82a010feff99fdb928a30b69ac3c5ef1701a2ba9
lucas-malla <lucasarien9210@gmail.com>
2024-12-12 00:06:37 +1100

    New

---
+++ src/main/java/practice/newSpringFramwwork/NewSpringFramwworkApplication.java
@@ -0,0 +1,13 @@
+package practice.newSpringFramwwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSpringFramwworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSpringFramwworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/newSpringFramwwork/NewSpringFramwworkApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.newSpringFramwwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSpringFramwworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
